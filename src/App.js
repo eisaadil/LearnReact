436e45af7169fc06254daf0cc5da58ce5d8cb8b1
Eisa Adil <eadil@blackberry.com>
2020-08-04 17:53:39 -0400

    Moved contact data to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './App.css';
 import './components/Contact'
 import Contact from './components/Contact';
+import contactData from "./components/contactData"
 
 function App() {
-  
+  const contacts = contactData.map(x => 
+    <div>
+        <h1>Name: {x.name}</h1>
+        <h2>Phone Num: {x.ph}</h2>
+        <h3>Email: {x.email}</h3>
+    </div>
+  )
   return (
     <div>
       {/* 
@@ -12,7 +19,7 @@ function App() {
       <Contact email="eisaadil@outlook.com" name="Eisa Adil" phoneNum = "2263408134"/>
       <Contact email="eisaadil@gmail.com" name="Eisa Adilz" phoneNum = "3533434"/>  */}
       {/* <Contact contactData = {{contactData}}/> */}
-      <Contact></Contact>
+      <Contact contactData = {contacts}></Contact>
     </div>
   );
 }
